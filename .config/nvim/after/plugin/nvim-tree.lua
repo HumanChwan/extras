b14b562a2926bbcd7eb965c3462e6bdec1a4fbd6
Dinesh Chukkala <dineshchukkala@wfh.local>
2024-09-07 14:39:00 +0530

    width update

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -9,7 +9,7 @@ require("nvim-tree").setup({
         sorter = "case_sensitive",
     },
     view = {
-        width = 40,
+        width = 35,
     },
     renderer = {
         group_empty = true,
